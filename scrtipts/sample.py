12b159e0d4c31c171b7f81abf63d021cb504d502
Vishwanatha BG <Vishwanatha.BG@ivanti.com>
2022-10-23 14:13:58 +0530

    machine ID extracting

--- scrtipts/sample.py
+++ scrtipts/sample.py
@@ -5,28 +5,17 @@ import sys
 import copy
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-auth = HTTPBasicAuth('admindb', 'dana123')
+auth = HTTPBasicAuth('admin', 'dana123')
 session = requests.Session()
 session.verify = False
 
-key = requests.get('https://10.96.206.17/api/v1/auth', auth=auth, verify=False).json()
+key = requests.get('https://10.96.224.60/api/v1/auth', auth=auth, verify=False).json()
 print(key)
 
 auth_info = session.get(
-        "https://10.96.206.17/api/v1/system/system-information",
+        "https://10.96.224.60/api/v1/system/system-information",
         auth=(key['api_key'], ""))
-print(auth_info.json())
+# print(auth_info.json())
 dictionary = auth_info.json()
+print(dictionary["system-information"]['machine-id'])
 
-class device_information:
-        def __init__(self, build):
-                self.build = build
-
-        def current(self):
-                print(f"current build :{self.build['software-inventory']['software']['build']}")
-
-        # def (self):
-        #         print(f"current build :{self.build['software-inventory']}")
-
-info = device_information(dictionary)
-info.current()
